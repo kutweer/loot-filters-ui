4594d557c009e742b24efb3ef323f22869f0c7c7
Brendan Ross <brendanros@gmail.com>
2025-03-09 22:03:44 -0500

    create util to download a file

---
+++ src/utils/file.ts
@@ -0,0 +1,15 @@
+export const downloadFile = (file: File) => {
+  const link = document.createElement("a");
+
+  try {
+    const href = URL.createObjectURL(file);
+    link.setAttribute('href', href)
+    link.setAttribute('download', file.name)
+    document.body.appendChild(link);
+    link.click();
+  } catch (error) {
+    console.error(`Failed to download file: ${file.name}`, error);
+  } finally {
+    document.body.removeChild(link);
+  }
+};
