676c6864609dc36330ff1ec9e5e89a9c8381b7e7
Kaqemeex <dev@kaqemeex.net>
2025-03-14 13:17:45 -0400

    move

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -9,7 +9,6 @@ import {
 } from '@mui/material'
 import { useUiStore } from '../store/store'
 import { colors } from '../styles/MuiTheme'
-import { ErrorBoundary } from './ErrorBoundary'
 
 export const Header: React.FC = () => {
     const { siteConfig, setSiteConfig } = useUiStore()
@@ -22,29 +21,27 @@ export const Header: React.FC = () => {
                     display: 'flex',
                 }}
             >
-                <ErrorBoundary>
-                    <Typography variant="h4" color="primary">
-                        Loot Filter Builder
-                        <Typography
-                            sx={{ paddingLeft: '1em', display: 'inline-block' }}
-                            gutterBottom
-                        >
-                            <span style={{ color: colors.rsYellow }}>
-                                A LootFilter builder for{' '}
-                                <Link
-                                    style={{
-                                        color: colors.rsYellow,
-                                        textDecorationColor: colors.rsYellow,
-                                    }}
-                                    target="_blank"
-                                    href="https://github.com/riktenx/loot-filters"
-                                >
-                                    RuneLite Loot Filters
-                                </Link>
-                            </span>
-                        </Typography>
+                <Typography variant="h4" color="primary">
+                    Loot Filter Builder
+                    <Typography
+                        sx={{ paddingLeft: '1em', display: 'inline-block' }}
+                        gutterBottom
+                    >
+                        <span style={{ color: colors.rsYellow }}>
+                            A LootFilter builder for{' '}
+                            <Link
+                                style={{
+                                    color: colors.rsYellow,
+                                    textDecorationColor: colors.rsYellow,
+                                }}
+                                target="_blank"
+                                href="https://github.com/riktenx/loot-filters"
+                            >
+                                RuneLite Loot Filters
+                            </Link>
+                        </span>
                     </Typography>
-                </ErrorBoundary>
+                </Typography>
                 {siteConfig.isLocal ? (
                     <FormControl sx={{ marginLeft: 'auto' }}>
                         <FormGroup>
