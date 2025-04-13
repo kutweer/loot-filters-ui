568f787dc5c626848fe2d72362180f78d57d4b8c
Kaqemeex <dev@kaqemeex.net>
2025-04-13 16:26:34 -0700

    update parser page

--- src/pages/ParsePage.tsx
+++ src/pages/ParsePage.tsx
@@ -1,5 +1,5 @@
 import { Editor } from '@monaco-editor/react'
-import { Button } from '@mui/material'
+import { Button, TextField, Typography } from '@mui/material'
 import { useMemo, useState } from 'react'
 import { ErrorBoundary } from '../components/ErrorBoundary'
 import { parse } from '../parsing/parse'
@@ -8,7 +8,9 @@ export const ParsePage = () => {
     const [editorContent, setEditorContent] = useState('')
     const [error, setError] = useState<Error | null>(null)
     const [url, setUrl] = useState(
-        'https://raw.githubusercontent.com/typical-whack/loot-filters-modules/refs/heads/migrate/filter.rs2f'
+        'https://raw.githubusercontent.com/riktenx/filterscape/refs/heads/migrate/filterscape.rs2f'
+        // 'https://raw.githubusercontent.com/typical-whack/loot-filters-modules/21b862c9502a20a82e0ac02038f92fb8c8f6bd4b/filter.rs2f'
+        // 'https://raw.githubusercontent.com/typical-whack/loot-filters-modules/refs/heads/migrate/filter.rs2f'
         // 'https://gist.githubusercontent.com/Kaqemeex/8b57b85303bf713167cbabe235638416/raw/58976caad9124bc99fba5ddec29932e266034e6f/foo.rs2f'
     )
 
@@ -31,8 +33,15 @@ export const ParsePage = () => {
 
     return (
         <ErrorBoundary>
-            <div style={{ padding: '1rem' }}>
-                <input
+            <div
+                style={{
+                    padding: '1rem',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '1rem',
+                }}
+            >
+                <TextField
                     type="text"
                     placeholder="Enter URL"
                     value={url}
@@ -41,13 +50,14 @@ export const ParsePage = () => {
                         padding: '0.5rem',
                         fontSize: '1rem',
                         borderRadius: '4px',
-                        border: '1px solid #ccc',
                     }}
                     onChange={(e) => {
                         setUrl(e.target.value)
                     }}
                 />
                 <Button
+                    variant="outlined"
+                    size="small"
                     onClick={() => {
                         fetch(url)
                             .then((res) => res.text())
@@ -57,35 +67,49 @@ export const ParsePage = () => {
                     Fetch
                 </Button>
             </div>
-            <Editor
-                height="50vh"
-                language="cpp"
-                theme="vs-dark"
-                options={{
-                    minimap: {
-                        enabled: false,
-                    },
-                    readOnly: false,
-                }}
-                value={editorContent}
-                onChange={(value) => {
-                    if (value) {
-                        setEditorContent(value)
-                    }
-                }}
-            />
-            <Editor
-                height="50vh"
-                language="json"
-                theme="vs-dark"
-                value={parsed}
-                options={{
-                    minimap: {
-                        enabled: false,
-                    },
-                    readOnly: true,
-                }}
-            />
+            <div style={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
+                <div style={{ width: '50%' }}>
+                    <Typography variant="h6" color="primary">
+                        Filter
+                    </Typography>
+                    <Editor
+                        height="80vh"
+                        width="100%"
+                        language="cpp"
+                        theme="vs-dark"
+                        options={{
+                            minimap: {
+                                enabled: false,
+                            },
+                            readOnly: false,
+                        }}
+                        value={editorContent}
+                        onChange={(value) => {
+                            if (value) {
+                                setEditorContent(value)
+                            }
+                        }}
+                    />
+                </div>
+                <div style={{ width: '50%' }}>
+                    <Typography variant="h6" color="primary">
+                        Parsed result
+                    </Typography>
+                    <Editor
+                        height="80vh"
+                        width="100%"
+                        language="json"
+                        theme="vs-dark"
+                        value={parsed}
+                        options={{
+                            minimap: {
+                                enabled: false,
+                            },
+                            readOnly: true,
+                        }}
+                    />
+                </div>
+            </div>
         </ErrorBoundary>
     )
 }
