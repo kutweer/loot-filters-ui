42b1b8839135b590122b83ae1963b13d86108d20
Kaqemeex <dev@kaqemeex.net>
2025-04-16 17:00:08 -0700

    cleanup

--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 import { Rs2fEditor } from '../components/Rs2fEditor'
 import { useEditorStore } from '../store/editor'
 
 export const EditorPage: React.FC = () => {
     const { contentById, setContent } = useEditorStore()
-    const [selected, setSelected] = useState<string | null>(null)
-
     return (
         <Rs2fEditor
             selected="editor"
             setEditorContent={setContent}
-            setSelected={setSelected}
+            setSelected={() => {}}
             filesContent={Object.fromEntries(
                 Object.entries(contentById).filter(([k, v]) => {
                     return k === 'editor'
