46fce7022cc33ec4bf11c99100a7d294a185edc8
typical-whack <11622150+typical-whack@users.noreply.github.com>
2025-04-25 17:15:43 -0400

    allow pasting comma delim list into stringlist input

--- packages/ui/src/components/inputs/StringListInputComponent.tsx
+++ packages/ui/src/components/inputs/StringListInputComponent.tsx
@@ -76,7 +76,15 @@ export const StringListInputComponent: React.FC<{
                     const values = ((newValue as Option[]) || []).map(
                         (option) => option.value
                     )
-                    onChange(convertToListDiff(values, input.default))
+                    const splitValues = values
+                        .map((v) =>
+                            v
+                                .split(',')
+                                .map((v) => v.trim())
+                                .filter((v) => v)
+                        )
+                        .flat()
+                    onChange(convertToListDiff(splitValues, input.default))
                 }}
             />
         </div>
